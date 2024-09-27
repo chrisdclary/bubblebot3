f9e283905510e4f97d3d781e1d442f6ceb102752
Chris Clary <chrisdennisclary@gmail.com>
2024-09-26 18:06:56 -0700

    Add support for bot slash commands

--- app/bubblebot.js
+++ app/bubblebot.js
@@ -1,15 +1,51 @@
-const auth = require('./auth.json')
-//const ytdl = require('ytdl-core')
-//const ytsr = require('ytsr')
-//const ytpl = require('ytpl')
+const fs = require('node:fs');
+const path = require('node:path');
+const auth = require('./auth.json');
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const cmdFolderPath = path.join(__dirname, 'cmd');
+const commandFiles = fs.readdirSync(cmdFolderPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(cmdFolderPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-
 client.login(auth.token);
+
